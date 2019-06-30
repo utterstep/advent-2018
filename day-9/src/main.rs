2aecca5152bb47a71b19f0ee13319a6cf9240056
Vlad Stepanov <8uk.8ak@gmail.com>
2019-06-30 16:00:00 +0300

    [infra] parametrize day-9 solution

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,34 +1,33 @@
 use std::error::Error;
 
-use advent_utils::{get_config, Part};
+use serde::Deserialize;
+
+use advent_utils::{get_custom_config, Part};
 
 mod game;
 
 use self::game::GameState;
 
-const N_PLAYERS: usize = 419;
-const MAX_MARBLE_PART_ONE: i64 = 71052;
-const MAX_MARBLE_PART_TWO: i64 = MAX_MARBLE_PART_ONE * 100;
+#[derive(Deserialize)]
+struct Config {
+    part: Part,
+    n_players: usize,
+    max_marbles: i64,
+}
 
 fn main() -> Result<(), Box<Error>> {
-    let config = get_config()?;
+    let config = get_custom_config::<Config>()?;
 
-    match config.part {
-        Part::One => {
-            let high_score = GameState::produce(N_PLAYERS, MAX_MARBLE_PART_ONE)
-                .high_score()
-                .unwrap();
+    let marbles_count = match config.part {
+        Part::One => config.max_marbles,
+        Part::Two => config.max_marbles * 100,
+    };
 
-            println!("highest score will be {}", high_score);
-        }
-        Part::Two => {
-            let high_score = GameState::produce(N_PLAYERS, MAX_MARBLE_PART_TWO)
-                .high_score()
-                .unwrap();
+    let high_score = GameState::produce(config.n_players, marbles_count)
+        .high_score()
+        .unwrap();
 
-            println!("highest score will be {}", high_score);
-        }
-    }
+    println!("highest score will be {}", high_score);
 
     Ok(())
 }
